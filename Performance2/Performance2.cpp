a4a8eb41c4cf7aa0ca3157d1f4c1e3df3c2fe729
0x24d <daniel.longfield@tuta.io>
2019-03-18 20:53:19 +0000

    Refactor logic before starting part 2

--- Performance2/Performance2.cpp
+++ Performance2/Performance2.cpp
@@ -106,88 +106,55 @@ CWinApp theApp;  // The one and only application object
 
 using namespace std;
 
-int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
-{
-	int nRetCode = 0;
+struct LogItem {
+    string sessionId;
+    string ipAddress;
+    string browser;
+    vector<string> paths;
+    vector<string> times;
+};
 
-	// initialize Microsoft Foundation Classes, and print an error if failure
-	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
-	{
-		_tprintf(_T("Fatal Error: MFC initialization failed\n"));
-		nRetCode = 1;
-	}
-	else
-	{
-		// Application starts here...
-
-		// Time the application's execution time.
-		TIMER start;	// DO NOT CHANGE THIS LINE. Timing will start here.
-
-		//--------------------------------------------------------------------------------------
-		// Insert your code from here...
-        struct LogItem {
-            string sessionId;
-            string ipAddress;
-            string browser;
-            vector<string> paths;
-            vector<string> times;
-        };
-        const string sessionStartTag = "<sessionid>";
-        const string sessionEndTag = "</sessionid>";
-        const string ipStartTag = "<ipaddress>";
-        const string ipEndTag = "</ipaddress>";
-        const string browserStartTag = "<browser>";
-        const string browserEndTag = "</browser>";
-        const string pathStartTag = "<path>";
-        const string pathEndTag = "</path>";
-        const string timeStartTag = "<time>";
-        const string timeEndTag = "</time>";
-
-        const string testFile = "testdata\\2";
-        ifstream xmlFile(testFile + ".xml");
-        string line;
-        vector<LogItem> logData;
-        // Parse XML file
-        while (getline(xmlFile, line)) {
-            LogItem item;
-            auto sessionStartTagBegin = line.find(sessionStartTag);
-            auto sessionEndTagBegin = line.find(sessionEndTag);
-            auto sessionIdBegin = sessionStartTagBegin + sessionStartTag.length();
-            string sessionId(line, sessionIdBegin, sessionEndTagBegin - sessionIdBegin);
-            item.sessionId = sessionId;
-            auto ipStartTagBegin = line.find(ipStartTag);
-            auto ipEndTagBegin = line.find(ipEndTag);
-            auto ipAddressBegin = ipStartTagBegin + ipStartTag.length();
-            string ipAddress(line, ipAddressBegin, ipEndTagBegin - ipAddressBegin);
-            item.ipAddress = ipAddress;
-            auto browserStartTagBegin = line.find(browserStartTag);
-            auto browserEndTagBegin = line.find(browserEndTag);
-            auto browserBegin = browserStartTagBegin + browserStartTag.length();
-            string browser(line, browserBegin, browserEndTagBegin - browserBegin);
-            item.browser = browser;
-            size_t pos = 0;
-            auto pathStartTagBegin = line.find(pathStartTag);
-            vector<string> paths;
-            vector<string> times;
-            while (pathStartTagBegin != string::npos) {
-                auto pathEndTagBegin = line.find(pathEndTag, pos);
-                auto pathBegin = pathStartTagBegin + pathStartTag.length();
-                string path(line, pathBegin, pathEndTagBegin - pathBegin);
-                paths.push_back(path);
-                auto timeStartTagBegin = line.find(timeStartTag, pos);
-                auto timeEndTagBegin = line.find(timeEndTag, pos);
-                auto timeBegin = timeStartTagBegin + timeStartTag.length();
-                string time(line, timeBegin, timeEndTagBegin - timeBegin);
-                times.push_back(time);
-                pos = timeEndTagBegin + timeEndTag.length();
-                pathStartTagBegin = line.find(pathStartTag, pos);
-            }
-            item.paths = paths;
-            item.times = times;
-            logData.push_back(item);
+class XMLParser {
+public:
+    LogItem parseLogLine(string line) {
+        LogItem item;
+        auto sessionStartTagBegin = line.find(sessionStartTag);
+        auto sessionEndTagBegin = line.find(sessionEndTag);
+        auto sessionIdBegin = sessionStartTagBegin + sessionStartTag.length();
+        string sessionId(line, sessionIdBegin, sessionEndTagBegin - sessionIdBegin);
+        item.sessionId = sessionId;
+        auto ipStartTagBegin = line.find(ipStartTag);
+        auto ipEndTagBegin = line.find(ipEndTag);
+        auto ipAddressBegin = ipStartTagBegin + ipStartTag.length();
+        string ipAddress(line, ipAddressBegin, ipEndTagBegin - ipAddressBegin);
+        item.ipAddress = ipAddress;
+        auto browserStartTagBegin = line.find(browserStartTag);
+        auto browserEndTagBegin = line.find(browserEndTag);
+        auto browserBegin = browserStartTagBegin + browserStartTag.length();
+        string browser(line, browserBegin, browserEndTagBegin - browserBegin);
+        item.browser = browser;
+        size_t pos = 0;
+        auto pathStartTagBegin = line.find(pathStartTag);
+        vector<string> paths;
+        vector<string> times;
+        while (pathStartTagBegin != string::npos) {
+            auto pathEndTagBegin = line.find(pathEndTag, pos);
+            auto pathBegin = pathStartTagBegin + pathStartTag.length();
+            string path(line, pathBegin, pathEndTagBegin - pathBegin);
+            paths.push_back(path);
+            auto timeStartTagBegin = line.find(timeStartTag, pos);
+            auto timeEndTagBegin = line.find(timeEndTag, pos);
+            auto timeBegin = timeStartTagBegin + timeStartTag.length();
+            string time(line, timeBegin, timeEndTagBegin - timeBegin);
+            times.push_back(time);
+            pos = timeEndTagBegin + timeEndTag.length();
+            pathStartTagBegin = line.find(pathStartTag, pos);
         }
-        xmlFile.close();
-        // Construct JSON file
+        item.paths = paths;
+        item.times = times;
+        return item;
+    }
+    string constructLogJson(vector<LogItem> logData) {
         auto logDataSize = logData.size();
         string outputJSON = "{\n";
         for (decltype(logDataSize) i = 0; i < logDataSize; ++i) {
@@ -214,10 +181,62 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
             outputJSON += "\n";
         }
         outputJSON += "}";
-        // Output to .json file
-        ofstream jsonFile(testFile + ".json");
-        jsonFile << outputJSON;
+        return outputJSON;
+    }
+    void outputToFile(string fileName, string json) {
+        ofstream jsonFile(fileName);
+        jsonFile << json;
         jsonFile.close();
+    }
+private:
+    const string sessionStartTag = "<sessionid>";
+    const string sessionEndTag = "</sessionid>";
+    const string ipStartTag = "<ipaddress>";
+    const string ipEndTag = "</ipaddress>";
+    const string browserStartTag = "<browser>";
+    const string browserEndTag = "</browser>";
+    const string pathStartTag = "<path>";
+    const string pathEndTag = "</path>";
+    const string timeStartTag = "<time>";
+    const string timeEndTag = "</time>";
+};
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	// initialize Microsoft Foundation Classes, and print an error if failure
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+		nRetCode = 1;
+	}
+	else
+	{
+		// Application starts here...
+
+		// Time the application's execution time.
+		TIMER start;	// DO NOT CHANGE THIS LINE. Timing will start here.
+
+		//--------------------------------------------------------------------------------------
+		// Insert your code from here...
+        
+        XMLParser parser;
+
+        const string testFile = "testdata\\2";
+        ifstream xmlFile(testFile + ".xml");
+        string line;
+        vector<LogItem> logData;
+        // Parse XML file
+        while (getline(xmlFile, line)) {
+            logData.push_back(parser.parseLogLine(line));
+        }
+        xmlFile.close();
+        // Construct JSON file
+        string outputJson = parser.constructLogJson(logData);
+        
+        // Output to .json file
+        parser.outputToFile(testFile + ".json", outputJson);
 
 		//-------------------------------------------------------------------------------------------------------
 		// How long did it take?...   DO NOT CHANGE FROM HERE...
